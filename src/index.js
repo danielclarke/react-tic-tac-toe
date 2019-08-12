83ac3ab8587cfb042ef45ff34fb53262ca219faa
daniel <dan137clarke@gmail.com>
2019-08-12 18:09:16 +1000

    game now tracks history

--- src/index.js
+++ src/index.js
@@ -11,37 +11,102 @@ function Square(props) {
 }
 
 class Board extends React.Component {
+    renderSquare(i) {
+        let value = this.props.getValue(i);
+        return <Square
+            n={i}
+            value={value}
+            onClick={() => {this.props.onClick(i)}}
+        />;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="status">{this.props.status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             player: 'X',
-            squares: Array(9).fill(" "),
+            history: [{squares: Array(9).fill(" ")}],
         }
     }
 
-    renderSquare(i) {
-        return <Square
-            n={i}
-            value={this.state.squares[i]}
-            onClick={() => {this.handleClick(i);}}
-        />;
+    renderBoard(status) {
+        return <Board 
+            status={status}
+            getValue={(i) => {return this.getValue(i)}}
+            onClick={(i) => {this.handleClick(i)}}
+        />
+    }
+
+    render() {
+        let status = 'Next player: ' + this.state.player;
+        const winningPlayer = this.checkWinner();
+        if (winningPlayer) {
+            status = 'Winner: ' + winningPlayer + "!";
+        }
+        return (
+            <div className="game">
+                <div className="game-board">
+                    {this.renderBoard(status)}
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+
+    getValue(i) {
+        let value = this.state.history[this.state.history.length - 1].squares[i];
+        return value;
     }
 
     handleClick(i) {
         if (this.checkWinner()) {
             return;
         }
-        let squares = this.state.squares.slice();
-        squares[i] = this.state.player;
-        this.setState({squares: squares}, () => {this.checkWinner()});
-        this.setState({player: this.state.player === 'X' ? 'O': 'X'});
+        if (this.state.history[this.state.history.length - 1].squares[i] === " ") {
+            let history = this.state.history.slice();
+            let squares = history[history.length - 1].squares.slice();
+            squares[i] = this.state.player;
+            history.push({squares: squares});
+            this.setState({history: history});
+            this.setState({player: this.state.player === 'X' ? 'O': 'X'});
+            console.log(history);
+        }
     }
 
     checkPlayerWinner(player) {
-        const state = this.state.squares.join("");
+        const state = this.state.history[this.state.history.length - 1].squares.join("");
         let winner = false;
         let winningStates = [
-            new RegExp([player, player, player].join("")),
+            new RegExp([player, player, player, "......"].join("")),
+            new RegExp(["...", player, player, player, "..."].join("")),
+            new RegExp(["......", player, player, player].join("")),
             new RegExp(["..", player, ".", player, ".", player, ".."].join("")),
             new RegExp([player, "..", player, "..", player].join("")),
             new RegExp([player, "...", player, "...", player].join("")),
@@ -65,51 +130,6 @@ class Board extends React.Component {
             return 'O';
         }
         return undefined;
-    }
-
-    render() {
-        let status;
-        if (this.checkWinner()) {
-            status = 'Winner: ' + this.state.player + "!";
-        } else {
-            status = 'Next player: ' + this.state.player;
-        }
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
     }
 }
 
