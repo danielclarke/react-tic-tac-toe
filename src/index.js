506dfa70b3bf017058cd19969390537634bae987
daniel <dan137clarke@gmail.com>
2019-08-13 08:12:50 +1000

    implemented time travel

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <div className="status">{this.props.status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -47,14 +46,13 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            player: 'X',
-            history: [{squares: Array(9).fill(" ")}],
+            stepNumber: 0,
+            history: [{squares: Array(9).fill(" "), player: 'X'}],
         }
     }
 
-    renderBoard(status) {
-        return <Board 
-            status={status}
+    renderBoard() {
+        return <Board
             getValue={(i) => {return this.getValue(i)}}
             onClick={(i) => {this.handleClick(i)}}
         />
@@ -62,39 +60,40 @@ class Game extends React.Component {
 
     render() {
         const winningPlayer = this.checkWinner();
-        const status = winningPlayer ? 'Winner: ' + winningPlayer + "!" : 'Next player: ' + this.state.player;
+        const status = winningPlayer ? 'Winner: ' + winningPlayer + "!" : 'Next player: ' + this.state.history[this.state.stepNumber].player;
         return (
             <div className="game">
                 <div className="game-board">
-                    {this.renderBoard(status)}
+                    {this.renderBoard()}
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{this.getMoves()}</ol>
                 </div>
             </div>
         );
     }
 
     getValue(i) {
-        return this.state.history[this.state.history.length - 1].squares[i];
+        return this.state.history[this.state.stepNumber].squares[i];
     }
 
     handleClick(i) {
         if (this.checkWinner()) {
             return;
         }
-        if (this.state.history[this.state.history.length - 1].squares[i] === " ") {
-            const history = this.state.history;
+        if (this.state.history[this.state.stepNumber].squares[i] === " ") {
+            const history = this.state.history.slice(0, this.state.stepNumber + 1);
             const squares = history[history.length - 1].squares.slice();
-            squares[i] = this.state.player;
-            this.setState({history: history.concat([{squares: squares}])});
-            this.setState({player: this.state.player === 'X' ? 'O': 'X'});
+            squares[i] = this.state.history[this.state.stepNumber].player;
+            const nextPlayer = this.state.history[this.state.stepNumber].player === 'X' ? 'O': 'X';
+            this.setState({history: history.concat([{squares: squares, player: nextPlayer}])});
+            this.setState({stepNumber: this.state.stepNumber + 1});
         }
     }
 
     checkPlayerWinner(player) {
-        const state = this.state.history[this.state.history.length - 1].squares.join("");
+        const state = this.state.history[this.state.stepNumber].squares.join("");
         const winningStates = [
             new RegExp([player, player, player, "......"].join("")),
             new RegExp(["...", player, player, player, "..."].join("")),
@@ -120,6 +119,20 @@ class Game extends React.Component {
             return 'O';
         }
         return undefined;
+    }
+
+    getMoves() {
+        const moveSelect = (move) => {
+            this.setState({stepNumber: move})
+        }
+        return this.state.history.map((step, move) => {
+            const desc = move ? `Go to move # ${move}` : "Go to game start";
+            return (
+                <li key={move}>
+                    <button onClick={() => {moveSelect(move)}}>{desc}</button>
+                </li>
+            );
+        });
     }
 }
 
